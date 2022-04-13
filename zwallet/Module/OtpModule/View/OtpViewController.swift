2b7c3f829bf3fd0c5b3fa3d782fb50cf477f32ff
bersihSekali <zangpangerancinta@gmail.com>
2022-04-12 22:27:34 -0700

    add border to OTP

--- zwallet/Module/OtpModule/View/OtpViewController.swift
+++ zwallet/Module/OtpModule/View/OtpViewController.swift
@@ -22,6 +22,38 @@ class OtpViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        pin1.delegate = self
+        pin2.delegate = self
+        pin3.delegate = self
+        pin4.delegate = self
+        pin5.delegate = self
+        pin6.delegate = self
+        
+        pin1.layer.borderWidth = 1
+        pin2.layer.borderWidth = 1
+        pin3.layer.borderWidth = 1
+        pin4.layer.borderWidth = 1
+        pin5.layer.borderWidth = 1
+        pin6.layer.borderWidth = 1
+        
+        pin1.layer.cornerRadius = 5
+        pin1.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMinXMinYCorner]
+        
+        pin2.layer.cornerRadius = 5
+        pin2.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMinXMinYCorner]
+        
+        pin3.layer.cornerRadius = 5
+        pin3.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMinXMinYCorner]
+        
+        pin4.layer.cornerRadius = 5
+        pin4.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMinXMinYCorner]
+        
+        pin5.layer.cornerRadius = 5
+        pin5.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMinXMinYCorner]
+        
+        pin6.layer.cornerRadius = 5
+        pin6.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMinXMinYCorner]
+        
         formWrapper.layer.cornerRadius = 50
         formWrapper.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
         
@@ -48,29 +80,39 @@ class OtpViewController: UIViewController, UITextFieldDelegate {
             switch textField {
             case pin1:
                 pin2.becomeFirstResponder()
+                pin1.layer.borderColor = UIColor.blue.cgColor
                 break
             case pin2:
                 pin3.becomeFirstResponder()
+                pin2.layer.borderColor = UIColor.blue.cgColor
                 break
             case pin3:
                 pin4.becomeFirstResponder()
+                pin3.layer.borderColor = UIColor.blue.cgColor
                 break
             case pin4:
                 pin5.becomeFirstResponder()
+                pin4.layer.borderColor = UIColor.blue.cgColor
                 break
             case pin5:
                 pin6.becomeFirstResponder()
+                pin5.layer.borderColor = UIColor.blue.cgColor
+                
+                break
+            case pin6:
+                pin6.resignFirstResponder()
+                pin6.layer.borderColor = UIColor.blue.cgColor
+                submitButton.titleLabel?.textColor = UIColor(ciColor: .white)
                 break
             default:
-                pin6.resignFirstResponder()
                 break
             }
             
             if (pin1.hasText && pin2.hasText && pin3.hasText && pin4.hasText && pin5.hasText && pin6.hasText) {
                 submitButton.backgroundColor = UIColor(named: "Primary")
                 submitButton.layer.cornerRadius = 10
-                submitButton.titleLabel?.textColor = UIColor(ciColor: .white)
                 submitButton.isEnabled = true
+                submitButton.titleLabel?.textColor = UIColor(ciColor: .white)
             }
         }
     }
@@ -107,6 +149,4 @@ extension OtpViewController: OtpViewProtocol {
         }))
         present(successAlert, animated: true, completion: nil)
     }
-    
-    
 }
